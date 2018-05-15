92eaf3ac461f12e8977e6a0167d7769b14f40004
Andrew Czachowski <czachandrew@gmail.com>
2018-05-14 20:37:41 -0500

    here is some new stuff to try

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -187,6 +187,7 @@ class CompanyController extends Controller
         
         ini_set('max_execution_time', '180');
         $file = $request->file('file');
+        return $request;
         $name = 'TestName';
         $ext = $file->getClientOriginalExtension();
         //$type = $this->getType($ext);
